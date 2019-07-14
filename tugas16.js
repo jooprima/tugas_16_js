1f19ac00f17f2aae17f85b00ddbecf483f3d0e45
yohanes_prima <jooprima@gmail.com>
2019-07-14 23:16:27 +0700

    Tugas 16 js
    tugas ini dikumpulkan untuk NIOMIC

---
+++ tugas16.js
@@ -0,0 +1,17 @@
+function minMax1() {
+  var min = Math.min(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10);
+  var max = Math.max(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10);
+  console.log("Nilai minimal : " + min);
+  console.log("Nilai maksimal : " + max);
+}
+minMax1();
+
+console.log("=======================================");
+
+function minMax2() {
+  var min = Math.min(45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5);
+  var max = Math.max(45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5);
+  console.log("Nilai minimal : " + min);
+  console.log("Nilai maksimal : " + max);
+}
+minMax2();
